9430334f44462e9b3ba2207dc9dde102eb2452d5
Adina Florea <adina.florea@fxbits.io>
2023-06-27 15:57:04 +0300

    showSkills from JS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var activePage = "home";
+var activePage = "skills";
 
 // utilities functions
 
@@ -44,7 +44,18 @@ function clickOnMenu(e) {
   }
 }
 
+function showSkills() {
+  var skills = ["HTML", "CSS", "JS"];
+  var htmlSkills = skills.map(function (skill) {
+    // <li class="favorite">HTML</li>
+    return `<li>${skill}</li>`;
+  });
+  var ul = $("#skills ul");
+  ul.innerHTML = htmlSkills.join("");
+}
+
 //start our code
 
 showPage(activePage);
 $("#top-menu-bar").addEventListener("click", clickOnMenu);
+showSkills();
